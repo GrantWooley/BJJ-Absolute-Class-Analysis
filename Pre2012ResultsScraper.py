ea4374d1443fe7b69b819a55027b34ef485f24ba
Grant Wooley <grantW900@yahoo.com>
2024-07-28 10:20:04 -0500

    Finished Pre2012 Scraper and updated TournamentResults scraper

--- Pre2012ResultsScraper.py
+++ Pre2012ResultsScraper.py
@@ -3,101 +3,121 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import pandas as pd
 
-#Example URL, will later turn .py file into function that accepts URL from TournamentResultsWebscraper.py
-URL = "https://ibjjf.com/events/results/2011-world-jiu-jitsu-ibjjf-championship"
-Page = urlopen(URL)
+#Defining file as function to call in Main TournamentResultsWebscraper file.
 
-Soup = BeautifulSoup(Page,"html.parser")
+def LegacyScrape(URL):
 
-AthleteResults = Soup.find("div", class_ = "col-sm-12 athletes")
+    #Open the connection and Get the HTML
+    Page = urlopen(URL)
+    Soup = BeautifulSoup(Page,"html.parser")
 
-#Splitting Out Tags that contain categories.
-Categories = AthleteResults.find_all("div", class_ ="category mt-4 mb-3")
+    #Pull out  HTML that includes all athlete result data, excludes Academy Results data.
+    AthleteResults = Soup.find("div", class_ = "col-sm-12 athletes")
 
-#Scrape through Categories tags. Pulling out Divsion string and splitting the strings properly
-#into age, belt, gender, weight
-#Declare a list to store Categories into. I.E. One instance of Age, Belt, Gender, Weight 
-# Is one cateogry.
-Headers = []
-for x in Categories:
-    #Split the category tag's .string into Age, Belt, Gender, Weight
-    NewCategory = x.string.split(" / ")
-    Headers.append(NewCategory)
+    #Splitting Out Tags that contain categories.
+    Categories = AthleteResults.find_all("div", class_ ="category mt-4 mb-3")
 
-
-#Split out Tags that contain Tables, that have two td elements. Td element one containing Athlete placing.
-#Td element two containing two cildren elements. 
-# Child1: Athelete Name, Child2: Academy Name
-ResultTables = AthleteResults.find_all("td")
-
-#Declare List to Store Placing information. Placing, Athlete Name, and Academy Name.
-Results = []
-#Declaring List that is used, to store one set of Placing Athlete Name, and Academy Name before being placed in the Results list.
-Sublist = []
-
-i = 0
-#Loop for seperating out Placing, Athlete Name, and Academy name.
-for tag in ResultTables:
-#    access tags attributes using tag['class'] and store in list
-    Checklist = tag['class']
-# access first element of list if the string is 'place'
-    if Checklist[0] == 'place':
-#Sometimes a division has 4 placing competitiors, sometimes it has 3. So, I need to check the placing. Everytime the placing = 1. wipe the sublist. and start over.
-#if the record is placing = 1 append the sublist to results, then wipe it, and add in the new record. This way each group of placings per division is stored in its own list, within the reuslts list.
-#checking for len of sublist so the first time through the loop we do not add an empty list to Results.
-        if tag.string == '1' and len(Sublist) != 0:
-            Results.append(Sublist)
-            Sublist = []
-#access Tag.str and add to results lists
-        Sublist.append(tag.string)        
-#else if it is the string 'athelete-academy'
-    elif Checklist[0] == 'athlete-academy':
-#search the tag for all div tags. 
-        DivTags = tag.find_all("div")
-#loop through each div tag and and add tag.string to results lists
-        for div in DivTags:
-            Sublist.append(div.string)
-#Appending Sublist to main results list on time outside of loop, because the way the loop works, the last group competitor results will be in the sublist but not appended to the results in the loop itself.
-Results.append(Sublist)
+    #Scrape through Categories tags. Pulling out Divsion string and splitting the strings properly
+    #into age, belt, gender, weight.
+    #Declare a list to store Categories into. I.E. One instance of Age, Belt, Gender, Weight 
+    #is one cateogry.
+    Headers = []
+    for x in Categories:
+        #One web page was missing a string attribute for a div tag. 
+        #If this happens insert a dummy header, as to not negatively affect ordering of finals dataframe results.
+        #Will likely not ever affect black belts results, if it does I will clean the final dataframe.
+        if x.string == None:
+            dummycategory = ['dummy1','dummy2','dummy3','dummy4']
+            Headers.append(dummycategory)
+            continue
+        #Split the category tag's .string into Age, Belt, Gender, Weight
+        NewCategory = x.string.split(" / ")
+        Headers.append(NewCategory)
 
 
+    #Split out Tags that contain Tbodies aka tables. These have two td children tags. Td tag one contains Athlete placing.
+    #Td tag two contains two cildren elements. 
+    # Child1: Athelete Name, Child2: Academy Name
+    ResultTables = AthleteResults.find_all("tbody")
 
-#Declaring my dataframe that I will now fill in with all of the scraped data.
-df = pd.DataFrame( columns=['Age','Belt','Gender','Weight Class','Placing','Competitor Name','Academy Name'])
+    #Declare List to Store Placing information. Placing, Athlete Name, and Academy Name.
+    Results = []
+    #Declaring List that is used, to store one set of Placing Athlete Name, and Academy Name before being placed in the Results list.
+    Sublist = []
 
-#I want to get one row of data. From my two lists. but my Results array has up to 4 rows worth of data in a single list.
-x = 0
-while x < len(Headers):
-    #access results list at the same position we are accessing headers list and store in a variable.
-    ResultsSplit = Results[x]
-    #A list object im getting from the Results array can be anywhere from 2 competitors to 4. But a result will always include 3 elements: Placing, Name, Academy Name.
-    #So I want to access the first 3 elements of the Results Split list. Add them to my new row, and then take them out of the ResultsSplit list until the results split list has nothing left in it.
-    while len(ResultsSplit) != 0:
-        NewRow = []
-        NewHeader = Headers[x]
-        for Header in NewHeader:
-            #Noticed some strings have white spaces. Stripping strings before storing.
-            NewRow.append(Header.strip()) 
-        IndiviualResult = ResultsSplit[0:3]    
-        for Result in IndiviualResult:
-            NewRow.append(Result.strip())
-        df.loc[len(df)] = NewRow
-        del ResultsSplit[0:3]
-        
-    x += 1
+    #Loop through each tbody.
+    for tag in ResultTables:
+        #Pull Out td children tags containing Placing, Athlete Name, and Academy Name.
+        TDtags = tag.find_all("td")
+        #Loop through TD tags and add elements to Sublist.
+        for tag in TDtags:
+            #Access td tags class and store in list variable.
+            x = tag['class']
+            x = x[0]
+            #If the td tag is a placing tag, add its text to sublist.
+            if x == 'place':
+                #Some web pages have errors where no placing was recorded. This returns none. Converting to string
+                #to prevent errors in later code.
+                Sublist.append(str(tag.string))
+            #Else if it is an athlete academy tag, go deeper into the html tree to get athlete name and academy.  
+            elif x == 'athlete-academy':
+                #Search the tag for all div tags. 
+                DivTags = tag.find_all("div")
+                #Loop through each div tag and and add tag.string to results lists.
+                for div in DivTags:
+                    #Some Atheletes do not have an affiliated academy, in those instances you get a return of none,
+                    #converting to string data type to prevent error.
+                    Sublist.append(str(div.string))
+        #Append Sublist Variable to Results list and then clear the sublist.
+        Results.append(Sublist)
+        Sublist = []
+
+    #Declaring empty dataframe to be filled with scraped data.
+    df = pd.DataFrame( columns=['Age','Belt','Gender','Weight Class','Placing','Competitor Name','Academy Name'])
+
+    #Looping through the two lists that contain data. Accessing them to build one row of data at a time and
+    #adding it to the data frame.
+    x = 0
+    while x < len(Headers):
+        #Access results list at the same position we are accessing headers list and store in a variable.
+        ResultsSplit = Results[x]
+        #A list object im getting from the Results array can be anywhere from 2 competitors to 4, but a result will always include 3 elements: Placing, Name, Academy Name.
+        #So I am accessing the first 3 elements of the Results Split list. Adding them to my new row, 
+        #and then taking them out of the ResultsSplit list until the results split list has nothing left in it.
+        while len(ResultsSplit) != 0:
+            NewRow = []
+            NewHeader = Headers[x]
+            for Header in NewHeader:
+                #Some strings have white spaces. Stripping strings before storing.
+                NewRow.append(Header.strip()) 
+            IndiviualResult = ResultsSplit[0:3]    
+            for Result in IndiviualResult:
+                NewRow.append(Result.strip())
+            df.loc[len(df)] = NewRow
+            del ResultsSplit[0:3]
+            
+        x += 1
 
 
-#Filtering df to only Black Belt, and Brown Black results (Female Divsions). As I only want to do analysis on the top level of competition.
-df = df[(df['Belt'] == 'Black') | (df['Belt'] == 'Brown Black')]
+    #Filtering df to only Adult divisions that contain Black Belt, 
+    #as analys will only be for top level competition.
+    #Female Divsions in early tournmaent years were combined belts. I.E. Brown Black, Purple Brown Black categories.
+    df = df[df['Belt'].str.contains('Black', na = False) ]
+    df = df[df['Age'] == 'Adult']
 
-df = df['Age']
-df = df.drop_duplicates()
+    #Using title element of the Soup to get year of tournament and adding to the data frame.
+    Title = Soup.title.string.split()
+    #Year is always the 3rd to last String in the title.
+    Year = Title[-3]
+    df['Year'] = Year
 
-print(df.head)
-print(df.tail)
+    #Using the title element of the Soup to get the tournament name and adding to the data frame.
+    Tournament = Soup.title.string
+    #Tournament name is always the title string until the 13th to last character.
+    #Accessing title string from the 0th position to the 13th to last position.
+    Tournament = Tournament[:-13]
+    df['Tournament'] = Tournament
 
-
-
-  
-
-
+    #Reseting df index and returning df.
+    df = df.reset_index(drop = True)
+    return df
--- TournamentResultsWebscraper.py
+++ TournamentResultsWebscraper.py
@@ -1,10 +1,11 @@
 #Webscraper Used for Scraping IBJJF Tournament Reulsts
 #For Worlds, Pans, Europeans, Brazilian Nationals, No-Gi Worlds, No-Gi Pans, No-Gi Europeans, & No-Gi Brazilian Nationals
-#Madle Black Belt Divisions
+#Male Black Belt Divisions
 
-#Import libraries beautiful soup urllib
+#Import libraries and Webscraper files
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import Pre2012ResultsScraper as OScraper
 
 #Open Connection to the primary IBJJF Event Results Webpage
 DirectoryURL = "https://ibjjf.com/events/results"
@@ -12,36 +13,48 @@ Page = urlopen (DirectoryURL)
 
 #Get HTML
 Soup = BeautifulSoup(Page, "html.parser")
-#Getting all tags that have an asscocitaed results URL
+#Getting all tags that have an asscocitaed results URLs
 tags = Soup.find_all('a',class_ = 'event-year-result')
 
-# List ofTournament names
+# List of Major Tournament names
 Tournaments =["World Jiu-Jitsu IBJJF Championship",
     "World Jiu-Jitsu No-Gi IBJJF Championship",
      "Pan Jiu-Jitsu IBJJF Championship",
      "Pan Jiu-Jitsu No-Gi IBJJF Championship",
      "European Jiu-Jitsu IBJJF Championship",
      "European Jiu-Jitsu No-Gi IBJJF Championship",
-     "European Jiu-Jitsu No-Gi IBJJF Championship"
+     "European Jiu-Jitsu No-Gi IBJJF Championship",
      "Campeonato Brasileiro de Jiu-Jitsu",
      "Brazilian National Jiu-Jitsu No-Gi Championship"]
 
 
+#Storing links to results pages from any tag that has a data-n elemnet matching a major tournament.
 ResultsURLs = [tag['href'] for tag in tags  if any(Tournament in tag['data-n'] for Tournament in Tournaments)]
-for x in ResultsURLs:
-    print(x)
-    
 
 
-#It looks like IBJJF has two web page layouts. Around the year 2012 the HTML changes. 
-#Need to figure out how to scrape both the old and the new web page formats for results data.
-#If URL ends in Public Results, use new scraping method, else use table scraping method.
+#IBJJF has two web page layouts. Around the year 2012 the HTML format/layout changed.
+#In the URLs for the results pages this is reflected. The older format web pages' URLs always end 
+#in the following format /year-tournammentname-ibjjf-championship. The newer web pages' URLs always
+#end wiht /PublicResults
 
-#Loop to begin opening connections to every IBJJF Results Page for major tournaments and fetching tournament results data.
-#for URL in ResultsURLs:
-#    ResultURL = URL
-#    ResultPage = urlopen (ResultURL)
-    #Get HTML
-#    ResultSoup = BeautifulSoup(ResultPage, "html.parser")
-    #print(ResultSoup.title)
+#Loop through URL in ResultsURL list. 
+#If the URL ends with PublicResults use the web scraping file set up for the new HTML format/layout
+#Else use the web scraping file set up for the old HTML format/layout
+
+#Loop opening connections to every IBJJF Results Page for major tournaments, scraping the webpage, returning a dataframe, and saving as CSV.
+i = 0
+for URL in ResultsURLs:
+    CheckURL = URL.rsplit("/",1)
+    if CheckURL[1] == 'PublicResults':
+        #Run Webscraper set up for new format.
+            pass
+    else:
+        #Run webscraper set up for old format.
+        print("Scraping: " + URL)
+        df = OScraper.LegacyScrape(URL)
+        File = URL.rsplit('/',1)
+        File = File[1]
+        df.to_excel(fr"C:\Users\grant\OneDrive\Road To DE\Data Projects\IBJJF Result Files\{File}.xlsx", sheet_name= "Results", index= False)
+        print(File + " saved.")
+
 
