52eaf90d2257ffeea565a74ca312085feebf72ab
grant <grantW900@yahoo.com>
2025-09-21 12:47:35 -0500

    Setting up file to contain common vars across scripts.

---
+++ R/Vars.R
@@ -0,0 +1,8 @@
+#Contains common Vars across R scripts.
+
+Path_Main <- file.path(getwd())
+Path_Data <- file.path(Path_Main,"data")
+Path_Data_Raw <- file.path(Path_Main,"data","raw_data")
+
+File_Results <- "IBJJF_Results_Weight_Class.rds"
+File_Absolute_Results <- "IBJJF_Results_Absolute.rds"
