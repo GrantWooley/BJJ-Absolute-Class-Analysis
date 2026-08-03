835a1a931591312d26911758fa5711a54880ac69
Grant W <grantW900@yahoo.com>
2026-08-03 00:45:36 -0500

    Style and Beautify both python files. Brining in line with pep 8. Making functions private. Restructuring for a natural readable flow of both files.

--- Python/scrape_results.py
+++ Python/scrape_results.py
@@ -1,18 +1,16 @@
-#Webscraper Used for Scraping IBJJF tournament results for all major tournaments.
-#Worlds, Pans, Europeans, Brazilian Nationals, No-Gi Worlds, No-Gi Pans, No-Gi Europeans, & No-Gi Brazilian Nationals
-#Male Black Belt Divisions
-
+# Webscraper used for Scraping IBJJF Male Black Belt division tournament results for all major tournaments.
+# Worlds, Pans, Europeans, Brazilian Nationals, No-Gi Worlds, No-Gi Pans, No-Gi Europeans, & No-Gi Brazilian Nationals
 
 from urllib.request import urlopen
 
 from bs4 import BeautifulSoup
 from pyhere import here
 
-#Import custom Webscraper functions
-import tournament_scrapers as sc
+# Import custom Webscraper functions
+from tournament_scrapers import scrape_legacy_web_page, scrape_modern_web_page
 
-#Major IBJJF Tournament names
-TOURNAMENTS =[
+# Major IBJJF Tournament names
+TOURNAMENTS = [
     "World Jiu-Jitsu IBJJF Championship",
     "World Jiu-Jitsu No-Gi IBJJF Championship",
     "Pan Jiu-Jitsu IBJJF Championship",
@@ -20,74 +18,79 @@ TOURNAMENTS =[
     "European Jiu-Jitsu IBJJF Championship",
     "European Jiu-Jitsu No-Gi IBJJF Championship",
     "Campeonato Brasileiro de Jiu-Jitsu",
-    "Brazilian National Jiu-Jitsu No-Gi Championship"
+    "Brazilian National Jiu-Jitsu No-Gi Championship",
 ]
 
-#Primary IBJJF Event Results Webpage
+# Primary IBJJF Event Results Webpage
 URL_RESULTS = "https://ibjjf.com/events/results"
 
-#pyhere used so project can run easily on any machine.
+# pyhere used so project can run easily on any machine.
 # PATH_DATA = here('data','raw_data')
-PATH_DATA = here('data','test')
+# FIXME
+PATH_DATA = here("data", "test")
 
-#Open URL and get beautiful soup object. Defining this function separately to allow
-#error handling of the URLs on the IBJFF Results page that are dead links.
-def get_main_results_page(URL):
-    Soup = urlopen(URL)
-    Soup = BeautifulSoup(Soup,"html.parser")
-    return Soup
 
-def get_individual_results_urls(soup):
-    #Getting all tags that have an associated results URLs
-    tags = soup.find_all('a',class_ = 'event-year-result')
-    #Storing links to results pages from any tag that has a data-n element matching a major tournament.
-    results_urls = [tag['href'] for tag in tags  if any(Tournament == tag['data-n'] for Tournament in TOURNAMENTS)]
+def _get_individual_results_urls(url):
+    # Get individual urls for all results web pages.
+
+    Soup = urlopen(url)
+    Soup = BeautifulSoup(Soup, "html.parser")
+
+    # Get tags that have an associated results URL.
+    tags = Soup.find_all("a", class_="event-year-result")
+    # Storing links to results pages from any tag that has a data-n element matching a major tournament.
+    results_urls = [
+        tag["href"]
+        for tag in tags
+        if any(Tournament == tag["data-n"] for Tournament in TOURNAMENTS)
+    ]
+
     return results_urls
 
-def scrape_webpage(tournament_url):
-    #IBJJF has two web page layouts. Around the year 2012 the HTML format/layout changed.
-    #In the URLs for the results pages this is reflected. The older format web pages' URLs always end
-    #in the following format /year-tournammentname-ibjjf-championship. The newer web pages' URLs always
-    #end with /PublicResults
-    check_url = current_url.rsplit("/",1)
 
-    #Select appropriate webscraping method based on url.
-    if check_url[1] == 'PublicResults':
+def _scrape_webpage(tournament_url):
+    # IBJJF has two web page layouts. Around the year 2012 the HTML format/layout changed.
+    # The older format web pages' URLs always end in the following format /year-tournammentname-ibjjf-championship.
+    # The newer web pages' URLs always end with /PublicResults
+    check_url = tournament_url.rsplit("/", 1)
 
-        df = sc.scrape_modern_web_page(Soup)
+    # Select appropriate webscraping method based on url.
+    if check_url[1] == "PublicResults":
+        df = scrape_modern_web_page(tournament_url)
         return df
 
     else:
-
-        df = sc.scrape_legacy_web_page(Soup)
+        df = scrape_legacy_web_page(tournament_url)
         return df
 
 
+def _save_tournament_results_file(df):
+    # Access first row of Tournament and Year columns to set the file name.
+    file_name = df.loc[0, "Tournament"] + " " + df.loc[0, "Year"]
+    df.to_csv(rf"{PATH_DATA}\{file_name}.csv", index=False)
 
-if __name__ == "__main__":
+    print(file_name + " saved.")
 
-    #Get main results page
-    Soup = get_main_results_page(URL_RESULTS)
-    #Get individual tournament results webpage urls.
-    results_urls = get_individual_results_urls(Soup)
 
-    #Scrape each webpage and save results as CSV file.
+def main():
+    # Get individual tournament results webpage urls.
+    results_urls = _get_individual_results_urls(URL_RESULTS)
+
+    # Scrape each webpage and save results as CSV file.
     for current_url in results_urls:
-
-        #Somre urls provided by results page are broken. Skip if unable to connect.
+        # Some urls provided by results page are broken. Skip if unable to connect.
         try:
-            Soup = get_main_results_page(current_url)
-        except:
-            print("Unable to connect to webpage, continuing to next URL.")
+            print("Scraping: " + current_url)
+            df = _scrape_webpage(current_url)
+
+        except Exception:
+            print("Unable to connect to webpage, continuing to next URL.\n{e}")
             continue
 
-        print("Scraping: " + current_url)
-        df = scrape_webpage(current_url)
-
-        #Access first row of Tournament and Year columns to set the file name.
-        file_name = df.loc[0,"Tournament"] +" " + df.loc[0,"Year"]
-        df.to_csv(fr"{PATH_DATA}\{file_name}.csv", index= False)
-
-        print(file_name + " saved.")
+        _save_tournament_results_file(df)
 
     print("All Web Pages Successfully Scraped!")
+
+
+if __name__ == "__main__":
+    main()
--- Python/tournament_scrapers.py
+++ Python/tournament_scrapers.py
@@ -1,176 +1,127 @@
-#Functions file contains helper function to Open web pages,
-#and two separate web scraping functions for scraping both the old and new HTML
-#format of the IBJJF results web pages.
+# tournament_scrapers file contains helper function two separate web scraping functions for scraping both the old and new HTML format of the IBJJF results web pages.
 
+from urllib.request import urlopen
 
-import pandas as pd
 import numpy as np
+import pandas as pd
+from bs4 import BeautifulSoup
 
 
-def scrape_legacy_web_page(Soup):
-    #Function for Scraping older HTML format of IBJFF pages. From roughly the year 2012 and before.
-    
-    #Pull out  HTML that includes all athlete result data, excludes Academy Results data.
-    athlete_results = Soup.find("div", class_ = "col-sm-12 athletes")
+def scrape_legacy_web_page(url):
+    # Function for Scraping older HTML format of IBJFF pages. From roughly the year 2012 and before.
 
-    #Get Division Information. I.e. adult, blue, male, middle
-    division_categories = collect_legacy_division_categories(athlete_results)
-    #Get Placing information. I.e. Placing, Athlete Name, and Academy Name.
-    results = collect_legacy_page_results(athlete_results)
+    Soup = _get_webpage_beautiful_soup(url)
 
-    full_rows = build_rows_of_data(division_categories, results)
-    df = create_standard_tournament_df(full_rows)
+    # Pull out  HTML that includes all athlete result data, excludes Academy Results data.
+    athlete_results = Soup.find("div", class_="col-sm-12 athletes")
 
-    df = filter_to_blackbelt_adult(df)
-    df['Tournament'] = get_legacy_tournament_name(Soup)
-    df['Year'] = get_legacy_tournament_year(Soup)
-    df = df.reset_index(drop = True)
+    # Get Division Information. I.e. adult, blue, male, middle
+    division_categories = _collect_legacy_division_categories(athlete_results)
+    # Get Placing information. I.e. Placing, Athlete Name, and Academy Name.
+    results = _collect_legacy_page_results(athlete_results)
 
-    return df
+    full_rows = _build_rows_of_data(division_categories, results)
+    df = _create_standard_tournament_df(full_rows)
 
-def scrape_modern_web_page(Soup):
-    #Function for scraping newer HTML format of IBJJF results pages. Roughly 2012 onward.
-    
-    #Pull out  HTML that includes all athlete result data, excludes Academy Results data.
-    athlete_results = Soup.find("div", class_ = "col-xs-12 col-md-6 col-athlete")
-
-    #Get Division Information. I.e. adult, blue, male, middle
-    division_categories = collect_modern_division_categories(athlete_results)
-    #Get Placing information. I.e. Placing, Athlete Name, and Academy Name.
-    results = collect_modern_page_results(athlete_results)
-
-    full_rows = build_rows_of_data(division_categories, results)
-    df = create_standard_tournament_df(full_rows)
-
-    df = filter_to_blackbelt_adult(df)
-
-    #Set year and tournament name using title.
-    page_title = get_modern_page_title(Soup) 
-    df['Year'] = page_title[-4:]
-    df['Tournament'] = page_title[:-4]
-
-    df = df.reset_index(drop = True)
+    df = _filter_to_blackbelt_adult(df)
+    df["Tournament"] = _get_legacy_tournament_name(Soup)
+    df["Year"] = _get_legacy_tournament_year(Soup)
+    df = df.reset_index(drop=True)
 
     return df
 
-def collect_legacy_division_categories(athlete_results):
+
+def scrape_modern_web_page(url):
+    # Function for scraping newer HTML format of IBJJF results pages. Roughly 2012 onward.
+
+    Soup = _get_webpage_beautiful_soup(url)
+
+    # Pull out  HTML that includes all athlete result data, excludes Academy Results data.
+    athlete_results = Soup.find("div", class_="col-xs-12 col-md-6 col-athlete")
+
+    # Get Division Information. I.e. adult, blue, male, middle
+    division_categories = _collect_modern_division_categories(athlete_results)
+    # Get Placing information. I.e. Placing, Athlete Name, and Academy Name.
+    results = _collect_modern_page_results(athlete_results)
+
+    full_rows = _build_rows_of_data(division_categories, results)
+    df = _create_standard_tournament_df(full_rows)
+
+    df = _filter_to_blackbelt_adult(df)
+
+    # Set year and tournament name using title.
+    page_title = _get_modern_page_title(Soup)
+    df["Year"] = page_title[-4:]
+    df["Tournament"] = page_title[:-4]
+
+    df = df.reset_index(drop=True)
+
+    return df
+
+
+def _get_webpage_beautiful_soup(url):
+    Soup = urlopen(url)
+    Soup = BeautifulSoup(Soup, "html.parser")
+
+    return Soup
+
+
+def _collect_legacy_division_categories(athlete_results):
     # Parse through legacy webpage athlete results data.
     # Pulling out division header info: age, belt, gender, weight.
     # Returning all division headers.
     # I.e. adult, blue, male, middle
     #      adult, blue, male, heavy
 
-    #Grab category tags that contain division info.
-    category_tags = athlete_results.find_all("div", class_ ="category mt-4 mb-3")
+    # Grab category tags that contain division info.
+    category_tags = athlete_results.find_all("div", class_="category mt-4 mb-3")
 
     divisions = []
     for category in category_tags:
-        #One web page was missing a string attribute for a div tag.
-        #If this happens insert a dummy header, as to not negatively affect ordering of finals dataframe results.
-        #Should not affect final df. If it does this is handled in data cleaning.
+        # One web page was missing a string attribute for a div tag.
+        # If this happens insert a dummy header, as to not negatively affect ordering of finals dataframe results.
+        # Should not affect final df. If it does this is handled in data cleaning.
         if category.string is None:
-            dummycategory = ['dummy1','dummy2','dummy3','dummy4']
+            dummycategory = ["dummy1", "dummy2", "dummy3", "dummy4"]
             divisions.append(dummycategory)
             continue
 
-        #Split the category tag into Age, Belt, Gender, Weight
+        # Split the category tag into Age, Belt, Gender, Weight
         category = category.string.split(" / ")
         divisions.append(category)
 
     return divisions
 
-def collect_modern_division_categories(athlete_results):
+
+def _collect_modern_division_categories(athlete_results):
     # Parse through legacy webpage athlete results data.
     # Pulling out division header info: age, belt, gender, weight.
     # Returning all division headers.
     # I.e. adult, blue, male, middle
     #      adult, blue, male, heavy
 
-    #Grab tags that contain division info.
-    category_tags =  athlete_results.find_all("h4", class_ = "subtitle")
+    # Grab tags that contain division info.
+    category_tags = athlete_results.find_all("h4", class_="subtitle")
 
     divisions = []
 
     for category in category_tags:
-
-        #Cateogry contents contain age, belt, gender, weight.
-        #Contents contain category info in multiple formats. Pull the best format.
+        # Category contents contain age, belt, gender, weight.
+        # Contents contain category info in multiple formats. Pull the best format.
         category_contents = category.contents
         category_contents = category_contents[1]
 
-        #Strings need to be split, reordered, and then stripped of line returns to get a clean division list.
+        # Strings need to be split, reordered, and then stripped of line returns to get a clean division list.
         category_contents = category_contents.split("/")
-        category_contents = reorder_modern_division_category(category_contents)
+        category_contents = _reorder_modern_division_category(category_contents)
         category_contents = [content.strip() for content in category_contents]
 
         divisions.append(category_contents)
 
     return divisions
 
-def collect_legacy_page_results(athlete_results):
-    # Pull out table objects from web page, and parse through them collecting results information.
 
-    # Grab Tags that contain Tbodies aka tables.
-    result_tables = athlete_results.find_all("tbody")
-
-    #List to Store Placing information. Placing, Athlete Name, and Academy Name.
-    results = []
-
-    for table in result_tables:
-        #Pull Out td children tags containing Placing, Athlete Name, and Academy Name.
-        td_tags = table.find_all("td")
-        individual_table_results = collect_individual_legacy_table_results(td_tags)
-
-        results.append(individual_table_results)
-
-    return results
-
-def collect_individual_legacy_table_results(td_tags):
-    #Collect all rows of an individual tournament results table object.
-
-    individual_table = []
-
-    #Go through tags collecting each row of Placing, Athlete Name, and Academy Name in table object.
-    for tag in td_tags:
-
-        #Access td tags class.
-        td_tag_class = tag['class'][0]
-
-        #Store Placing information.
-        if td_tag_class == 'place':
-            # Some web pages have errors where no placing was recorded.
-            # Returns none. Convert to string to prevent errors in later code.
-            individual_table.append(str(tag.string))
-
-        #If the tag is an athlete academy tag, parse the html tree further to get athlete name and academy.
-        elif td_tag_class == 'athlete-academy':
-
-            div_tags = tag.find_all("div")
-
-            #Collect and Store Athlete Name and Academy Name information.
-            for div in div_tags:
-                #Some Athletes do not have an affiliated academy, in those instances you get a return of none,
-                #converting to string data type to prevent error.
-                individual_table.append(str(div.string))
-
-    return individual_table
-
-def collect_modern_page_results(athlete_results):
-    # Pull out table objects from web page, and parse through them collecting results information.
-
-    #Stores Placing information. I.e. Placings, Athlete Names, and Academy Names.
-    results = []
-
-    # List tags are table like objects.
-    result_tables = athlete_results.find_all("div", class_ = "list")
-
-    for current_table in result_tables:
-        individual_table_results = collect_individual_modern_table_results(current_table)
-        results.append(individual_table_results)
-
-    return results
-
-def reorder_modern_division_category(category_contents):
+def _reorder_modern_division_category(category_contents):
     # Reorder contents of the category so it matches the order used in the legacy web pages.
     # I.e. Change order of Age, Gender, Belt, Weight Class to Age, Belt, Gender, Weight Class
     # Allows for reuse of more functions across both web page formats.
@@ -182,21 +133,88 @@ def reorder_modern_division_category(category_contents):
 
     return category_contents
 
-def collect_individual_modern_table_results(table):
-    #Collect all rows of a modern webpage individual tournament results table object.
+
+def _collect_legacy_page_results(athlete_results):
+    # Pull out table objects from web page, and parse through them collecting results information.
+
+    # Grab Tags that contain Tbodies aka tables.
+    result_tables = athlete_results.find_all("tbody")
+
+    # List to Store Placing information. Placing, Athlete Name, and Academy Name.
+    results = []
+
+    for table in result_tables:
+        # Pull Out td children tags containing Placing, Athlete Name, and Academy Name.
+        td_tags = table.find_all("td")
+        individual_table_results = _collect_individual_legacy_table_results(td_tags)
+
+        results.append(individual_table_results)
+
+    return results
+
+
+def _collect_individual_legacy_table_results(td_tags):
+    # Collect all rows of an individual tournament results table object.
 
     individual_table = []
 
-    #Pull Out Athlete Item tags. I.e. Rows of the table.
-    athlete_items = table.find_all("div", class_ = "athlete-item")
+    # Go through tags collecting each row of Placing, Athlete Name, and Academy Name in table object.
+    for tag in td_tags:
+        # Access td tags class.
+        td_tag_class = tag["class"][0]
+
+        # Store Placing information.
+        if td_tag_class == "place":
+            # Some web pages have errors where no placing was recorded.
+            # Returns none. Convert to string to prevent errors in later code.
+            individual_table.append(str(tag.string))
+
+        # If the tag is an athlete academy tag, parse the html tree further to get athlete name and academy.
+        elif td_tag_class == "athlete-academy":
+            div_tags = tag.find_all("div")
+
+            # Collect and Store Athlete Name and Academy Name information.
+            for div in div_tags:
+                # Some Athletes do not have an affiliated academy, in those instances you get a return of none,
+                # converting to string data type to prevent error.
+                individual_table.append(str(div.string))
+
+    return individual_table
+
+
+def _collect_modern_page_results(athlete_results):
+    # Pull out table objects from web page, and parse through them collecting results information.
+
+    # Stores Placing information. I.e. Placings, Athlete Names, and Academy Names.
+    results = []
+
+    # List tags are table like objects.
+    result_tables = athlete_results.find_all("div", class_="list")
+
+    for current_table in result_tables:
+        individual_table_results = _collect_individual_modern_table_results(
+            current_table
+        )
+        results.append(individual_table_results)
+
+    return results
+
+
+def _collect_individual_modern_table_results(table):
+    # Collect all rows of a modern webpage individual tournament results table object.
+
+    individual_table = []
+
+    # Pull Out Athlete Item tags. I.e. Rows of the table.
+    athlete_items = table.find_all("div", class_="athlete-item")
 
     for item in athlete_items:
-        #Get tag that contains Placing
-        placing = item.find("div",class_ = "position-athlete")
+        # Get tag that contains Placing
+        placing = item.find("div", class_="position-athlete")
         placing = placing.contents[0]
         individual_table.append(placing.strip())
 
-        #p tags contains both athlete and school name
+        # p tags contains both athlete and school name
         p_tag = item.find("p")
 
         athlete_name = p_tag.contents[0]
@@ -205,39 +223,35 @@ def collect_individual_modern_table_results(table):
         academy_name = p_tag.find("span")
         individual_table.append(academy_name.string.strip())
 
-
     return individual_table
 
-def build_rows_of_data(division_categories, results):
-    #Go through both our division categories and results.
-    #Combining them into full rows of data for final dataframe creation.
+
+def _build_rows_of_data(division_categories, results):
+    # Go through both our division categories and results.
+    # Combining them into full rows of data for final dataframe creation.
 
     full_rows = []
 
     x = 0
     while x < len(division_categories):
-
-        #Access results list at the same position we are accessing division_categories list and store.
+        # Access results list at the same position we are accessing division_categories list and store.
         results_split = results[x]
 
-        #A list object im getting from the results array can be anywhere from 2 competitors to 4, but will always include 3 elements: Placing, Name, Academy Name.
+        # A list object im getting from the results array can be anywhere from 2 competitors to 4, but will always include 3 elements: Placing, Name, Academy Name.
 
-        #Access the first 3 elements of the results split list and add them to the new row.
-        #Remove the elements out of the results split list until the results split list has nothing left in it.
+        # Access the first 3 elements of the results split list and add them to the new row.
+        # Remove the elements out of the results split list until the results split list has nothing left in it.
         while len(results_split) != 0:
-
-            #Make this dic
             new_row = []
 
             current_category = division_categories[x]
             for category in current_category:
-                #Clean Strings before storing, some had whitespaces.
+                # Clean Strings before storing, some had whitespaces.
                 new_row.append(category.strip())
 
             individual_result = results_split[0:3]
             for result in individual_result:
                 new_row.append(result.strip())
-
 
             full_rows.append(new_row)
 
@@ -247,47 +261,64 @@ def build_rows_of_data(division_categories, results):
 
     return full_rows
 
-def create_standard_tournament_df(rows_of_data):
-    df = pd.DataFrame(rows_of_data,
-                      columns=['Age','Belt','Gender','Weight Class','Placing','Competitor Name','Academy Name']
-                      )
+
+def _create_standard_tournament_df(rows_of_data):
+    df = pd.DataFrame(
+        rows_of_data,
+        columns=[
+            "Age",
+            "Belt",
+            "Gender",
+            "Weight Class",
+            "Placing",
+            "Competitor Name",
+            "Academy Name",
+        ],
+    )
     return df
 
-def filter_to_blackbelt_adult(df):
+
+def _filter_to_blackbelt_adult(df):
 
     # Analysis is for top level competition. Black Belt, Adult
-    
+
     # Use contains instead of exact match as Female Divisions in early tournament years were combined belts.
     # I.E. Brown Black, Purple Brown Black categories.
     # Some tournament results are stored in Portuguese. Filter for both English & Portuguese.
-    df = df[(df['Belt'].str.contains('Black', na = False)) | (df['Belt'].str.contains('Preta', na = False))]
-    
-    #On one web page, Adult is misspelled as Asult. Filtering for this page and correcting these records.
-    df = df[(df['Age'] == 'Adult') | (df['Age'] == 'Adulto') | (df['Age'] == 'Asult')]
-    df['Age'] = np.where(df['Age'] == 'Asult', 'Adult', df['Age'])
-    
+    df = df[
+        (df["Belt"].str.contains("Black", na=False))
+        | (df["Belt"].str.contains("Preta", na=False))
+    ]
+
+    # On one web page, Adult is misspelled as Asult. Filtering for this page and correcting these records.
+    df = df[(df["Age"] == "Adult") | (df["Age"] == "Adulto") | (df["Age"] == "Asult")]
+    df["Age"] = np.where(df["Age"] == "Asult", "Adult", df["Age"])
+
     return df
 
-def get_legacy_tournament_name(Soup):
-    #Using the title element of the Soup to get the tournament name.
+
+def _get_legacy_tournament_name(Soup):
+    # Using the title element of the Soup to get the tournament name.
     tournament_name = Soup.title.string
 
-    #Tournament name is always the title string until the 13th to last character.
+    # Tournament name is always the title string until the 13th to last character.
     tournament_name = tournament_name[:-13]
     return tournament_name
 
-def get_legacy_tournament_year(Soup):
-    #Using title element of the Soup to get year of the tournament.
 
-    #Title element contains both year of tournament and tournament name.
-    #Year is always the 3rd to last String in the title.
+def _get_legacy_tournament_year(Soup):
+    # Using title element of the Soup to get year of the tournament.
+
+    # Title element contains both year of tournament and tournament name.
+    # Year is always the 3rd to last String in the title.
     tournament_year = Soup.title.string.split()
     tournament_year = tournament_year[-3]
     return tournament_year
 
-def get_modern_page_title(Soup):
+
+def _get_modern_page_title(Soup):
     # Getting the title that contains both tournament name and year.
-    title = Soup.find_all("h2",class_ = "title")
+    title = Soup.find_all("h2", class_="title")
     title = title[1]
     title = title.string.strip()
 
