6444fa97aeb428720afe35bf66675b014959cece
Grant W <grantW900@yahoo.com>
2026-08-09 23:29:16 -0500

    Clean up comments.

--- R/0.1_Setup.R
+++ R/0.1_Setup.R
@@ -1,8 +1,8 @@
-#Contains common Vars across R scripts.
+# Contains common variables used across R scripts and Quarto files.
 
+# Packages need to be loaded outside in Setup file regardless of main file, for when file gets called by Quarto.
 library(here)
-#forcats needs to be inlcuded in setup file. During Quarto file render, one of the plot objects that are
-#loaded has a forcats function that is called in the plot object.
+# forcats needs to be inlcuded in setup file. During Quarto file render, one of the plot objects that is loaded has a forcats function that is called in the plot object.
 library(forcats)
 
 Path_Main <- file.path(here())
@@ -14,8 +14,8 @@ Path_Plots <- file.path(Path_Data,"plots")
 File_Results <- "IBJJF_Results_Weight_Class.rds"
 File_Absolute_Results <- "IBJJF_Results_Absolute.rds"
 
-#Scoping issue with scales::percent() requires these vars to be loaded before pulling plot objects.
-#Variables used to control scale_percent label size and accuracy across multiple plots
+# Scoping issue with scales::percent() requires these vars to be loaded before pulling plot objects.
+# Variables used to control scale_percent label size and accuracy across multiple plots
 Percent_Size <- 3.3
 Percent_Accuracy <- 0.1
 
