86cc3f511d71e8eb99c5625b4f88957485c01a65
Grant W <grantW900@yahoo.com>
2026-08-09 22:53:26 -0500

    Create Main file used for script orchestration.

---
+++ R/0.0_Main.R
@@ -0,0 +1,21 @@
+library(data.table)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(stringr)
+library(scales)
+library(forcats)
+library(purrr)
+library(here)
+
+
+print("Set up environment.")
+source(here("R", "0.1_Setup.R"))
+
+print("Clean Raw Data")
+source(here("R", "1.0_Data_Cleansing.R"))
+
+print("Run Analysis and Produce Plots")
+source(here("R", "2.0_IBJJF_Analysis.R"))
+
+print("Analysis finsihed, ready for Quarto to be ran.")
