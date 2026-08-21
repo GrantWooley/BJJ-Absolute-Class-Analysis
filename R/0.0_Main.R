80da6ce210dbbc691c19a7be0e388133db0e04d5
Grant W <grantW900@yahoo.com>
2026-08-21 00:20:23 -0500

    Rename data quality check file.

--- R/0.0_Main.R
+++ R/0.0_Main.R
@@ -21,6 +21,6 @@ print("Run Analysis and Produce Plots")
 source(here("R", "2.0_IBJJF_Analysis.R"))
 
 print("Run Data Quality Checks")
-source(here("R", "3.0_DQC.R"))
+source(here("R", "3.0_Data_Quality_Checks.R"))
 
 print("Analysis finsihed, ready for Quarto site to be rendered.")
